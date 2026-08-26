f0009a95a418268880826ce79a7f31dbda77f569
Bhargav-Sadineni <sadinenibhargav0@gmail.com>
2026-08-26 19:19:57 +0530

    checking

--- client/context/Authcontext.jsx
+++ client/context/Authcontext.jsx
@@ -6,6 +6,7 @@ import {io} from "socket.io-client"
 const backendUrl = import.meta.env.VITE_BACKEND_URL;
 axios.defaults.baseURL = backendUrl;
 
+
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children })=>{
